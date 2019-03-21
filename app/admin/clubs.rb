0bc7891cfe3e9c79517962bf47769afcb4825868
Raghu Betina <raghu@firstdraft.com>
2019-03-21 14:51:19 +0000

    Whitelist columns in Club for ActiveAdmin

--- app/admin/clubs.rb
+++ app/admin/clubs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Club do
+
+ permit_params :league_id, :name, :level
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
