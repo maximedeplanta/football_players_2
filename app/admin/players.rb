536094c45495d2d07cc35f2e31620636309c8e3d
Raghu Betina <raghu@firstdraft.com>
2019-03-21 14:51:24 +0000

    Whitelist columns in Player for ActiveAdmin

--- app/admin/players.rb
+++ app/admin/players.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Player do
+
+ permit_params :club_id, :name, :position, :level
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
